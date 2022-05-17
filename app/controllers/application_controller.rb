f3e6e74e8f4a66d2ba6b48ab8f47296a3bd7bf1f
jimmyray92 <jameswang675203@gmail.com>
2022-05-17 23:23:55 +1000

    added pagy and faker gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    include Pagy::Backend
+    include Pagy::Frontend
     before_action :authenticate_user!
 
     def after_sign_in_path_for(resource)
---
+++ config/initializers/pagy.rb
@@ -0,0 +1,6 @@
+# Pagy::DEFAULT[:items] = 25
+# Pagy::VARS[:cycle] = true
+
+require 'pagy/extras/bootstrap'
+require 'pagy/extras/overflow'
+# Pagy::VARS[:overflow] = :last_page
